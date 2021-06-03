b3dca931c8ee4ee08eebd58880072679be1ab98e
Jordan <redlion-20212000@protonmail.com>
2021-06-02 21:19:55 -0500

    Adelanto del homework 4

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -80,6 +80,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+	var suma = 0;
+  	for ( var i = 0; i < numeros.length; i++ ) {
+    		suma = suma + numeros [ i ];
+  	}
+  	return suma;
 }
 
 
@@ -87,6 +92,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+	return agregarNumeros ( resultadosTest ) / resultadosTest.length;
 }
 
 
@@ -94,6 +100,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+	var maximo = numeros [ 0 ];
+ 	for ( var i = 1; i < numeros.length; i++ ) {
+    		if ( numeros [ i ] > maximo ) { 
+      			maximo = numeros [ i ];
+    		}
+  	}
+  	return maximo;
 }
 
 
@@ -101,6 +114,7 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+	
 }
 
 
